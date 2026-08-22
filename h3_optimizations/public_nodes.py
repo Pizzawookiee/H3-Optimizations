675ddd52f242e7ad8331890d6511226e08de0a2f
Zironic <zironicum@gmail.com>
2026-08-22 14:22:36 +0200

    Hide experimental MLP nodes from public registration

---
+++ h3_optimizations/public_nodes.py
@@ -0,0 +1,20 @@
+'''Public ComfyUI node registration for H3 Optimizations.''' 
+
+from comfy_api.latest import ComfyExtension
+
+from .nodes import (
+    H3MemoryOptimization,
+    H3SparseAttention,
+    H3SparseAttentionAdvanced,
+)
+
+
+class H3OptimizationsExtension(ComfyExtension):
+    '''Register only production-ready H3 optimization nodes.'''
+
+    async def get_node_list(self):
+        return [
+            H3MemoryOptimization,
+            H3SparseAttention,
+            H3SparseAttentionAdvanced,
+        ]
