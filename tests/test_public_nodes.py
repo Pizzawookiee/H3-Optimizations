1c94957681ad824bd2dc7caaf198095ff96a70f0
Zironic <zironicum@gmail.com>
2026-08-25 09:10:56 +0200

    Test precision mode migration

--- tests/test_public_nodes.py
+++ tests/test_public_nodes.py
@@ -20,8 +20,13 @@ import comfy.options  # noqa: E402
 comfy.options.enable_args_parsing()
 
 from h3_optimizations.aimdo_limiter import H3AIMDOResidencyLimiter  # noqa: E402
-from h3_optimizations.nodes import (  # noqa: E402
+from h3_optimizations.memory_migration_node import (  # noqa: E402
     H3MemoryOptimization,
+    PRECISION_MODE_ALLOW_FP8,
+    PRECISION_MODE_PRESERVE,
+    _preserve_precision_for_mode,
+)
+from h3_optimizations.nodes import (  # noqa: E402
     H3SparseAttention,
     H3SparseAttentionAdvanced,
     _memory_request,
@@ -90,6 +95,23 @@ class PublicNodeTests(unittest.TestCase):
         )
         self.assertEqual(request.fused_qkv, FUSED_QKV_PRESERVE_BF16)
         self.assertEqual(request.mlp_memory, MLP_MEMORY_OFF)
+
+    def test_precision_mode_is_authoritative(self):
+        self.assertTrue(_preserve_precision_for_mode(PRECISION_MODE_PRESERVE))
+        self.assertFalse(_preserve_precision_for_mode(PRECISION_MODE_ALLOW_FP8))
+
+    def test_memory_schema_keeps_hidden_legacy_slot_before_new_mode(self):
+        schema = H3MemoryOptimization.define_schema()
+        inputs = schema.inputs
+        ids = [item.id for item in inputs]
+        legacy_index = ids.index('preserve_precision')
+        mode_index = ids.index('precision_mode')
+        self.assertEqual(mode_index, legacy_index + 1)
+
+        legacy = inputs[legacy_index]
+        mode = inputs[mode_index]
+        self.assertTrue(legacy.extra_dict.get('hidden'))
+        self.assertEqual(mode.default, PRECISION_MODE_PRESERVE)
 
 
 if __name__ == '__main__':
