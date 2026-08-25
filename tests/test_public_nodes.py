2bc0d3a241f4535dba47e5979b49349ab99ea482
Zironic <zironicum@gmail.com>
2026-08-25 09:55:19 +0200

    Test QKV streaming mode policy

--- tests/test_public_nodes.py
+++ tests/test_public_nodes.py
@@ -24,7 +24,12 @@ from h3_optimizations.memory_migration_node import (  # noqa: E402
     H3MemoryOptimization,
     PRECISION_MODE_ALLOW_FP8,
     PRECISION_MODE_PRESERVE,
+    QKV_STREAMING_MODE_AUTO,
+    QKV_STREAMING_MODE_FORCED,
+    QKV_STREAMING_MODE_OFF,
+    _memory_request_for_modes,
     _preserve_precision_for_mode,
+    _qkv_streaming_request,
 )
 from h3_optimizations.nodes import (  # noqa: E402
     H3SparseAttention,
@@ -32,10 +37,15 @@ from h3_optimizations.nodes import (  # noqa: E402
     _memory_request,
 )
 from h3_optimizations.plan import (  # noqa: E402
+    ATTENTION_AUTO,
     ATTENTION_EXISTING,
+    FUSED_QKV_OFF,
     FUSED_QKV_PRESERVE_BF16,
     MLP_MEMORY_OFF,
     MLP_MEMORY_PRESERVE,
+    QKV_STREAMING_AUTO,
+    QKV_STREAMING_FORCED,
+    QKV_STREAMING_OFF,
 )
 from h3_optimizations.public_nodes import H3OptimizationsExtension  # noqa: E402
 
@@ -100,18 +110,77 @@ class PublicNodeTests(unittest.TestCase):
         self.assertTrue(_preserve_precision_for_mode(PRECISION_MODE_PRESERVE))
         self.assertFalse(_preserve_precision_for_mode(PRECISION_MODE_ALLOW_FP8))
 
-    def test_memory_schema_keeps_hidden_legacy_slot_before_new_mode(self):
+    def test_qkv_streaming_mode_maps_to_plan_values(self):
+        self.assertEqual(_qkv_streaming_request(QKV_STREAMING_MODE_OFF), QKV_STREAMING_OFF)
+        self.assertEqual(_qkv_streaming_request(QKV_STREAMING_MODE_AUTO), QKV_STREAMING_AUTO)
+        self.assertEqual(_qkv_streaming_request(QKV_STREAMING_MODE_FORCED), QKV_STREAMING_FORCED)
+
+    def test_streaming_auto_claims_unselected_attention(self):
+        request = _memory_request_for_modes(
+            fused_qkv='auto',
+            mlp_memory='auto',
+            chunk_rows=2048,
+            precision_mode=PRECISION_MODE_PRESERVE,
+            qkv_streaming_mode=QKV_STREAMING_MODE_AUTO,
+            explicit_attention_selected=False,
+        )
+        self.assertEqual(request.attention, ATTENTION_AUTO)
+        self.assertEqual(request.fused_qkv, FUSED_QKV_PRESERVE_BF16)
+        self.assertEqual(request.qkv_streaming, QKV_STREAMING_AUTO)
+
+    def test_streaming_auto_yields_to_explicit_attention(self):
+        request = _memory_request_for_modes(
+            fused_qkv='auto',
+            mlp_memory='auto',
+            chunk_rows=2048,
+            precision_mode=PRECISION_MODE_PRESERVE,
+            qkv_streaming_mode=QKV_STREAMING_MODE_AUTO,
+            explicit_attention_selected=True,
+        )
+        self.assertEqual(request.attention, ATTENTION_EXISTING)
+        self.assertEqual(request.fused_qkv, FUSED_QKV_PRESERVE_BF16)
+        self.assertEqual(request.qkv_streaming, QKV_STREAMING_AUTO)
+
+    def test_streaming_forced_claims_attention_even_when_explicit(self):
+        request = _memory_request_for_modes(
+            fused_qkv='auto',
+            mlp_memory='auto',
+            chunk_rows=2048,
+            precision_mode=PRECISION_MODE_PRESERVE,
+            qkv_streaming_mode=QKV_STREAMING_MODE_FORCED,
+            explicit_attention_selected=True,
+        )
+        self.assertEqual(request.attention, ATTENTION_AUTO)
+        self.assertEqual(request.qkv_streaming, QKV_STREAMING_FORCED)
+
+    def test_streaming_off_preserves_attention_and_disables_qkv_projector(self):
+        request = _memory_request_for_modes(
+            fused_qkv='auto',
+            mlp_memory='auto',
+            chunk_rows=2048,
+            precision_mode=PRECISION_MODE_PRESERVE,
+            qkv_streaming_mode=QKV_STREAMING_MODE_OFF,
+        )
+        self.assertEqual(request.attention, ATTENTION_EXISTING)
+        self.assertEqual(request.fused_qkv, FUSED_QKV_OFF)
+        self.assertEqual(request.qkv_streaming, QKV_STREAMING_OFF)
+
+    def test_memory_schema_appends_streaming_after_precision_mode(self):
         schema = H3MemoryOptimization.define_schema()
         inputs = schema.inputs
         ids = [item.id for item in inputs]
         legacy_index = ids.index('preserve_precision')
-        mode_index = ids.index('precision_mode')
-        self.assertEqual(mode_index, legacy_index + 1)
+        precision_index = ids.index('precision_mode')
+        streaming_index = ids.index('qkv_streaming_mode')
+        self.assertEqual(precision_index, legacy_index + 1)
+        self.assertEqual(streaming_index, precision_index + 1)
 
         legacy = inputs[legacy_index]
-        mode = inputs[mode_index]
+        precision = inputs[precision_index]
+        streaming = inputs[streaming_index]
         self.assertTrue(legacy.extra_dict.get('hidden'))
-        self.assertEqual(mode.default, PRECISION_MODE_PRESERVE)
+        self.assertEqual(precision.default, PRECISION_MODE_PRESERVE)
+        self.assertEqual(streaming.default, QKV_STREAMING_MODE_AUTO)
 
 
 if __name__ == '__main__':
